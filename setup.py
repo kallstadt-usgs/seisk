9514953d35b94d56ad4e0885ff057600b8909b93
kallstadt <kallstadt@usgs.gov>
2015-11-16 15:32:14 -0700

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
-setup(name='idontknowyet',
+setup(name='seisk',
       version='0.1dev',
-      description='stuff',
+      description='Codes for downloading data and interacting with it using obspy',
       author='Kate Allstadt',
       author_email='kallstadt@usgs.gov',
       url='',
